846d1e75f6a3d545798edaf5a4773ed1eb7b421a
Samson-Mayeem <63527034+Samson-Mayeem@users.noreply.github.com>
2022-11-22 07:00:15 +0000

    halfway jwt commits

---
+++ Core.App.Api/Context/ContextCon.cs
@@ -0,0 +1,16 @@
+﻿using Core.App.Api.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.App.Api.Context
+{
+    public class ContextCon : DbContext
+    {
+        public ContextCon(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Product> products { get; set; }
+        public DbSet<News> news { get; set; }
+       public DbSet<Payment> payments { get; set; }    
+    }
+}
---
+++ Core.App.Api/Data/JwtAuthManager.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.App.Api.Data
+{
+    public class JwtAuthManager
+    {
+        private readonly string _apiAuthKey;
+        private readonly JwtAuthManager _jwtAuthManager;   
+        private readonly IDictionary<string, string> _authUserKeys = new Dictionary<string, string>()
+        {
+            {"test","password"},
+            {"test1","pwd"}
+        };
+        public JwtAuthManager(string Key)
+        {
+           this._apiAuthKey = Key;
+        }   
+    }
+}
